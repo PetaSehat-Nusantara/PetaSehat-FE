190b095e397e036d2fa1ab0a790f982b7a18905f
ghinaa2703 <hafizmuh18@gmail.com>
2025-06-01 05:00:09 +0700

    deploy

--- src/app/genkit/nusa-cari.ts
+++ src/app/genkit/nusa-cari.ts
@@ -1,25 +1,25 @@
-'use server';
-import { gemini15Flash, googleAI } from '@genkit-ai/googleai';
-import { genkit, z } from 'genkit';
-
-const ai = genkit({
-  plugins: [googleAI()],
-});
-
-export const healthCheck = ai.defineFlow(
-  {
-    name: 'healthCheck',
-    inputSchema: z.object({ data: z.string() }),
-    outputSchema: z.object({ message: z.string() }),
-    streamSchema: z.string(),
-  },
-  async ({ data }) => {
-    const response = await ai.generate({
-      model: gemini15Flash,
-      prompt: `This is a health check, say something funny about ${data} if you are alive.`,
-    });
-
-    const { text } = response;
-    return { message: text };
-  }
-);
+// 'use server';
+// import { gemini15Flash, googleAI } from '@genkit-ai/googleai';
+// import { genkit, z } from 'genkit';
+//
+// const ai = genkit({
+//   plugins: [googleAI()],
+// });
+//
+// export const healthCheck = ai.defineFlow(
+//   {
+//     name: 'healthCheck',
+//     inputSchema: z.object({ data: z.string() }),
+//     outputSchema: z.object({ message: z.string() }),
+//     streamSchema: z.string(),
+//   },
+//   async ({ data }) => {
+//     const response = await ai.generate({
+//       model: gemini15Flash,
+//       prompt: `This is a health check, say something funny about ${data} if you are alive.`,
+//     });
+//
+//     const { text } = response;
+//     return { message: text };
+//   }
+// );
